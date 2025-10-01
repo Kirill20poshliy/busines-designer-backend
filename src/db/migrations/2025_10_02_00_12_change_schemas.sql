49b98142c28a5161e2f42513e45c153a036c6d7b
Kirill20poshliy <qazasd2014@yandex.ru>
2025-10-02 00:41:42 +0300

    fix: migration #2

--- src/db/migrations/2025_10_02_00_12_change_schemas.sql
+++ src/db/migrations/2025_10_02_00_12_change_schemas.sql
@@ -1,8 +1,8 @@
 DO $$
 DECLARE
-    never_id INT;
-    periodic_id INT;
-    replacement_id INT;
+    never_id INTEGER;
+    periodic_id INTEGER;
+    replacement_id INTEGER;
 BEGIN
     IF NOT EXISTS (
         SELECT 1
@@ -14,57 +14,43 @@ BEGIN
         ADD COLUMN is_started BOOLEAN NOT NULL DEFAULT FALSE;
     END IF;
 
-    SELECT 
-        id INTO never_id 
-    FROM trigger_types 
-    WHERE name = 'Никогда';
-
-    SELECT 
-        id INTO periodic_id 
-    FROM trigger_types 
-    WHERE name = 'Периодично';
+    SELECT id INTO never_id FROM trigger_types WHERE name = 'Никогда';
+    SELECT id INTO periodic_id FROM trigger_types WHERE name = 'Периодично';
     
     SELECT id INTO replacement_id 
     FROM trigger_types 
     WHERE name NOT IN ('Никогда', 'Периодично') 
     LIMIT 1;
 
-    IF replacement_id IS NOT NULL THEN
-        IF never_id IS NOT NULL THEN
+    IF never_id IS NOT NULL THEN
+        IF replacement_id IS NOT NULL THEN
             UPDATE documents 
-            SET trigger_type_id = replacement_id 
-            WHERE trigger_type_id = never_id;
-
-            DELETE FROM trigger_types 
-            WHERE id = never_id;
-        END IF;
-
-        IF periodic_id IS NOT NULL THEN
+            SET trigger_type = replacement_id 
+            WHERE trigger_type = never_id;
+        ELSE
             UPDATE documents 
-            SET trigger_type_id = replacement_id 
-            WHERE trigger_type_id = periodic_id;
-
-            DELETE FROM trigger_types 
-            WHERE id = periodic_id;
+            SET trigger_type = NULL 
+            WHERE trigger_type = never_id;
         END IF;
-    ELSE
-        IF never_id IS NOT NULL THEN
-            UPDATE documents 
-            SET trigger_type_id = NULL 
-            WHERE trigger_type_id = never_id;
-
-            DELETE FROM trigger_types 
-            WHERE id = never_id;
-        END IF;
-
-        IF periodic_id IS NOT NULL THEN
-            UPDATE documents 
-            SET trigger_type_id = NULL 
-            WHERE trigger_type_id = periodic_id;
-
-            DELETE FROM trigger_types 
-            WHERE id = periodic_id;
-        END IF;
+        
+        DELETE FROM trigger_types 
+        WHERE id = never_id;
     END IF;
+
+    IF periodic_id IS NOT NULL THEN
+        IF replacement_id IS NOT NULL THEN
+            UPDATE documents 
+            SET trigger_type = replacement_id 
+            WHERE trigger_type = periodic_id;
+        ELSE
+            UPDATE documents 
+            SET trigger_type = NULL 
+            WHERE trigger_type = periodic_id;
+        END IF;
+        
+        DELETE FROM trigger_types 
+        WHERE id = periodic_id;
+    END IF;
+
 END;
 $$;
