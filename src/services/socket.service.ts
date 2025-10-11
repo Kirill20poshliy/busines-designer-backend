51a3aac0d09346c7e1233d1e1230d03ce0592a72
Kirill20poshliy <qazasd2014@yandex.ru>
2025-10-11 19:22:44 +0300

    chore: remove comments

--- src/services/socket.service.ts
+++ src/services/socket.service.ts
@@ -177,19 +177,6 @@ export class SocketService {
                 }
             )
 
-            // authSocket.on(
-            //     "give-agent-executing-status",
-            //     (data: IAgentExecutingStatusUpdate) => {
-            //         try {
-            //             this.handleGetAgentExecuteingStatus(authSocket, data);
-            //         } catch (error) {
-            //             authSocket.emit("error", {
-            //                 message: "Failed to give agent executing status",
-            //             });
-            //         }
-            //     }
-            // )
-
             authSocket.on(
                 "get-agent-logs",
                 (data: IAgentLogsQuery) => {
@@ -296,25 +283,6 @@ export class SocketService {
 
         this.updateUserActivity(documentId, socket.userId);
     }
-
-    // private async handleGetAgentExecuteingStatus(
-    //     socket: IAuthenticatedSocket,
-    //     data: IAgentExecutingStatusUpdate,
-    // ) {
-    //     const { documentId } = data;
-
-    //     const agentsManager = AgentsManager.getInstance();
-    //     const isRunning = agentsManager.isAgentExecuting(documentId);
-
-    //     this.io!.to(documentId).emit("get-agent-executing-status", {
-    //         documentId,
-    //         isRunning,
-    //         userId: socket.userId,
-    //         timestamp: new Date().toISOString(),
-    //     })
-
-    //     this.updateUserActivity(documentId, socket.userId);
-    // }
 
     private async handleGetAgentLogs(
         socket: IAuthenticatedSocket,
